77aaaa7d7c27b7d797722e23e519720c3b3c55d8
ampersand <ampersand.dev@gmail.com>
2021-02-22 13:01:47 +0900

    14のE2Eテスト完了

--- src/components/squares.js
+++ src/components/squares.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 export function Square(props) {
-    const style = {color:'red'}
-    return (
-        <button className="square"  data-cy={`square-${props.number}`}  style={style} onClick={props.onClick}>
-            {props.value}
-        </button>
-    );
+  const style = { color: "red" };
+  return (
+    <button
+      data-cy={`square-${props.number}`}
+      style={style}
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
